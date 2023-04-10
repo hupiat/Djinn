e7c25b472ac5c54f38a484d61cb6cc74b458338a
Hugo Piat <h.piat@outlook.fr>
2023-04-10 23:19:17 +0200

    front : mv fetch -> middleware

--- client/src/components/equipments/ViewEquipments/index.tsx
+++ client/src/components/equipments/ViewEquipments/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Loader } from "rsuite";
-import { useStoreDataEquipments } from "../../../commons/fetch/hooks.tsx";
+import { useStoreDataEquipments } from "../../../commons/middleware/hooks.tsx";
 
 export default function ViewEquipments() {
   const [data, store] = useStoreDataEquipments();
