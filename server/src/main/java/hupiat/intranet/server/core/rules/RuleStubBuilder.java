9125dfa34fe9ecd671769e2cee80fa21d759caa8
Hugo Piat <h.piat@outlook.fr>
2023-05-08 15:24:54 +0200

    fix

--- server/src/main/java/hupiat/intranet/server/core/rules/RuleStubBuilder.java
+++ server/src/main/java/hupiat/intranet/server/core/rules/RuleStubBuilder.java
@@ -9,7 +9,7 @@ import com.google.common.collect.ImmutableMap;
 
 import hupiat.intranet.server.core.rules.generics.SizeRuled;
 
-public record RuleStubBuilder<T extends RuleStubProxiable>(Class<T> clazz) implements Serializable {
+record RuleStubBuilder<T extends RuleStubProxiable>(Class<T> clazz) implements Serializable {
 
 	private static final ImmutableMap<String, ? extends RuleStubProxiable> constraints = ImmutableMap.of(
 			RuleStubProxiable.TEXT, new SizeRuled(SizeRuled.MIN_VALUE), RuleStubProxiable.TEXT_SHORT,
@@ -17,7 +17,7 @@ public record RuleStubBuilder<T extends RuleStubProxiable>(Class<T> clazz) imple
 			RuleStubProxiable.mutatedProxy(RuleStubProxiable.TEXT_SHORT),
 			new SizeRuled(SizeRuled.MIN_VALUE_ALT, SizeRuled.MAX_VALUE_COMMON));
 
-	public Map<String, T> build() {
+	Map<String, T> build() {
 		var res = new LinkedHashMap<String, T>();
 		var predicate = Predicates.instanceOf(clazz);
 		for (var entry : constraints.entrySet()) {
