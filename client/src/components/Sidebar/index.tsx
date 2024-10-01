b856b12f0951458d3d951dbf22723c9f173bdd70
Hugo Piat <h.piat@outlook.fr>
2024-10-01 22:09:14 +0200

    implemented logout

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -7,23 +7,31 @@ import {
 } from "@mui/material";
 import React, { useState } from "react";
 import "./styles.css";
-import { PATH_PROJECTS } from "../../commons/middleware/paths";
-import { Map } from "@mui/icons-material";
+import { PATH_LOGIN, PATH_PROJECTS } from "../../commons/middleware/paths";
+import { Logout, Map } from "@mui/icons-material";
+import Logo from "../../assets/logo.webp";
+import { useMiddlewareContext } from "../../commons/middleware/context";
 
 export default function Sidebar() {
   const [selected, setSelected] = useState<number>(0);
+  const { setUser } = useMiddlewareContext();
 
   const renderItem = (
     title: string,
     path: string,
     icon: JSX.Element,
-    index: number
+    index: number,
+    onClick?: () => void
   ) => {
     return (
       <ListItemButton
         selected={index === selected}
         onClick={() => {
           setSelected(index);
+          onClick && onClick();
+          // not using BrowserRouter here to build the
+          // Sidebar above (better components manage flow)
+          window.location.href = path;
         }}
       >
         <ListItemIcon>{icon}</ListItemIcon>
@@ -34,7 +42,13 @@ export default function Sidebar() {
 
   return (
     <Box id="sidebar__container">
+      <Box id="sidebar__top__container">
+        <img src={Logo} alt="Logo Djinn" /> <h1>Djinn</h1>
+      </Box>
       <List>{renderItem("Projects", PATH_PROJECTS, <Map />, 0)}</List>
+      <List>
+        {renderItem("Logout", PATH_LOGIN, <Logout />, 1, () => setUser(null))}
+      </List>
     </Box>
   );
 }
