923beec94deedc8d58e54a50d88bfddd178560ee
Hugo Piat <h.piat@outlook.fr>
2024-11-10 10:40:34 +0100

    Worked on navbar

--- client/src/commons/middleware/paths.ts
+++ client/src/commons/middleware/paths.ts
@@ -2,5 +2,5 @@ export const API_PREFIX = "api";
 export const API_ACCOUNTS = "accounts";
 
 export const PATH_LOGIN = "/";
-export const PATH_CV_LIST = "cvs";
-export const PATH_SETTINGS = "settings";
+export const PATH_CV_LIST = "/cvs";
+export const PATH_SETTINGS = "/settings";
--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,13 +1,53 @@
-import { Card } from "@mui/material";
-import React from "react";
+import { Box, Card, Tab, Tabs } from "@mui/material";
+import React, { useState } from "react";
 import "./styles.css";
 import Logo from "../../assets/logo.webp";
+import { HistoryEdu, Settings } from "@mui/icons-material";
+import { useLocation, useNavigate } from "react-router-dom";
+import { PATH_CV_LIST, PATH_SETTINGS } from "../../commons/middleware/paths";
 
 export default function Navbar() {
+  const location = useLocation();
+  const getTabIndexInitial = (): number => {
+    switch (location.pathname) {
+      case PATH_CV_LIST:
+        return 0;
+      case PATH_SETTINGS:
+        return 1;
+      default:
+        return 0;
+    }
+  };
+
+  const [tab, setTab] = useState<number>(getTabIndexInitial());
+  const navigate = useNavigate();
+
+  const handleChange = (_: React.SyntheticEvent, newValue: number) => {
+    setTab(newValue);
+    switch (newValue) {
+      case 0:
+        navigate(PATH_CV_LIST);
+        break;
+      case 1:
+        navigate(PATH_SETTINGS);
+        break;
+    }
+  };
+
   return (
     <Card id="navbar">
-      <img src={Logo} alt="Logo" />
-      <h1>Djinn CV Builder</h1>
+      <Box>
+        <img src={Logo} alt="Logo" />
+        <h1>Djinn</h1>
+      </Box>
+      <Tabs value={tab} onChange={handleChange}>
+        <Tab
+          label="My curriculums"
+          icon={<HistoryEdu />}
+          iconPosition="start"
+        />
+        <Tab label="Settings" icon={<Settings />} iconPosition="start" />
+      </Tabs>
     </Card>
   );
 }
